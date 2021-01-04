8e5dfeba843ce2a4d3681fef0aa713cb5ce64e46
Dawid Leszczyński <dawid.leszczynski@gmail.com>
2021-01-04 16:29:31 +0100

    Refactor Media::Medium read model

--- app/read_models/media/medium.rb
+++ app/read_models/media/medium.rb
@@ -14,26 +14,18 @@ module Media
       def call(event)
         case event
         when Curation::PhotoRegistered
-          medium = find_or_initialize_by(id: event.data[:uid])
-          medium.status = 'registered'
-          medium.save!
+          set_status(event.data[:uid], 'registered')
         when Curation::PhotoRejected
-          medium = find_or_initialize_by(id: event.data[:uid])
-          medium.status = 'rejected'
-          medium.save!
+          set_status(event.data[:uid], 'rejected')
         when Curation::PhotoPublished
           medium = find_or_initialize_by(id: event.data[:uid])
           medium.status = 'published'
           medium.publish_at = event.data[:publish_at]
           medium.save!
         when Curation::PhotoCopyrightFound
-          medium = find_or_initialize_by(id: event.data[:uid])
-          medium.copyright = 'found'
-          medium.save!
+          set_copyright(event.data[:uid], 'found')
         when Curation::PhotoCopyrightNotFound
-          medium = find_or_initialize_by(id: event.data[:uid])
-          medium.copyright = 'ok'
-          medium.save!
+          set_copyright(event.data[:uid], 'ok')
         end
       end
 
@@ -43,6 +35,20 @@ module Media
         Rails.configuration.event_store.read.forward.of_type(EVENTS).each do |event|
           self.call(event)
         end
+      end
+
+      private
+
+      def set_status(uid, status)
+        medium = find_or_initialize_by(id: uid)
+        medium.status = status
+        medium.save!
+      end
+
+      def set_copyright(uid, copyright)
+        medium = find_or_initialize_by(id: uid)
+        medium.copyright = copyright
+        medium.save!
       end
     end
   end
