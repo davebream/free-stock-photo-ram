04aea074f40cd4bb2eb2db25f6b68b46e98c07d8
Dawid Leszczyński <dawid.leszczynski@gmail.com>
2021-09-23 00:59:50 +0200

    Remove redundant methods from Review::Photo

--- app/read_models/review/photo.rb
+++ app/read_models/review/photo.rb
@@ -5,19 +5,5 @@ module Review
     def published?
       publish_at.present?
     end
-
-    def self.set_status(id, status)
-      set_attribute(id, :status, status)
-    end
-
-    def self.set_copyright(id, status)
-      set_attribute(id, :copyright, status)
-    end
-
-    def self.set_attribute(id, attribute, value)
-      photo = find_or_initialize_by(id: id)
-      photo[attribute] = value
-      photo.save!
-    end
   end
 end
