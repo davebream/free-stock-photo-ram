2321d746386bf114742d04357eff42be26897b4e
Dawid Leszczyński <dawid.leszczynski@gmail.com>
2021-09-22 19:07:59 +0200

    Extract ApprovePhoto service

--- app/controllers/review_controller.rb
+++ app/controllers/review_controller.rb
@@ -7,7 +7,9 @@ class ReviewController < ApplicationController
 
   def reject
     with_transaction do
-      command_bus.call(reject_photo_command)
+      command_bus.call(
+        Reviewing::RejectPhoto.new(photo_id: params[:id], correlation_id: params[:id])
+      )
     end
 
     redirect_to action: 'index'
@@ -15,44 +17,22 @@ class ReviewController < ApplicationController
 
   def pre_approve
     with_transaction do
-      command_bus.call(pre_approve_photo_command)
+      command_bus.call(
+        Reviewing::PreApprovePhoto.new(photo_id: params[:id], correlation_id: params[:id])
+      )
     end
 
     redirect_to action: 'index'
   end
 
   def approve
-    with_transaction do
-      command_bus.call(approve_photo_command)
+    result = ApprovePhoto.new.call(params[:id])
+
+    if result.failure?
+      flash.keep
+      flash[:error] = result.failure[:message]
     end
 
     redirect_to action: 'index'
-  rescue StandardError => e
-    flash.keep
-
-    case e
-      when Reviewing::Photo::NotYetPreApproved
-        flash[:error] = 'Photo not yet pre approved'
-      when Reviewing::Photo::HasBeenRejected
-        flash[:error] = 'Approving rejected photos forbidden'
-      else
-        raise e
-    end
-
-    redirect_to action: 'index'
-  end
-
-  private
-
-  def reject_photo_command
-    Reviewing::RejectPhoto.new(photo_id: params[:id], correlation_id: params[:id])
-  end
-
-  def pre_approve_photo_command
-    Reviewing::PreApprovePhoto.new(photo_id: params[:id], correlation_id: params[:id])
-  end
-
-  def approve_photo_command
-    Reviewing::ApprovePhoto.new(photo_id: params[:id], correlation_id: params[:id])
   end
 end
