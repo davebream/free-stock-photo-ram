65edef59ebad683758f5ac53071aaffb8e3b0ff2
Dawid Leszczyński <dawid.leszczynski@gmail.com>
2021-11-23 12:01:28 +0100

    Change domain_events directories to events

--- config/initializers/zeitwerk.rb
+++ config/initializers/zeitwerk.rb
@@ -2,10 +2,10 @@ SUBDOMAINS = %w(reviewing file_processing tagging copyright_checking publishing)
 
 Rails.autoloaders.each do |autoloader|
   SUBDOMAINS.each do |sub|
-    domain_events_dir = Rails.root.join("#{sub}/lib/#{sub}/domain_events")
+    events_dir = Rails.root.join("#{sub}/lib/#{sub}/events")
     commands_dir = Rails.root.join("#{sub}/lib/#{sub}/commands")
 
-    autoloader.collapse(domain_events_dir)
+    autoloader.collapse(events_dir)
     autoloader.collapse(commands_dir)
   end
 end
