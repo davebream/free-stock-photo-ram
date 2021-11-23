263db50d6c9313ba13e9f10d8e5f1e5bfad4206c
Dawid Leszczyński <dawid.leszczynski@gmail.com>
2021-11-23 12:45:26 +0100

    Simplify base Event class

--- lib/event.rb
+++ lib/event.rb
@@ -1,62 +1,30 @@
-class Event < Dry::Struct
-  transform_keys(&:to_sym)
+require 'active_support/core_ext/hash'
 
-  def self.new(data: {}, metadata: {}, **rest)
-    super(rest.merge(data).merge(metadata: metadata))
+class Event < RubyEventStore::Event
+  module WithSchema
+    class Schema < Dry::Struct
+      transform_keys(&:to_sym)
+    end
+
+    module ClassMethods
+      extend Forwardable
+      def_delegators :schema, :attribute, :attribute?
+
+      def schema
+        @schema ||= Class.new(Schema)
+      end
+    end
+
+    module Constructor
+      def initialize(event_id: SecureRandom.uuid, metadata: nil, data: {})
+        super(event_id: event_id, metadata: metadata, data: data.deep_merge(self.class.schema.new(data).to_h))
+      end
+    end
+
+    def self.included(klass)
+      klass.extend  WithSchema::ClassMethods
+      klass.include WithSchema::Constructor
+    end
   end
-
-  def self.inherited(klass)
-    super
-    klass.attribute :metadata, Types.Constructor(RubyEventStore::Metadata).default { RubyEventStore::Metadata.new }
-    klass.attribute :event_id, Types::UUID.default { SecureRandom.uuid }
-  end
-
-  def timestamp
-    metadata[:timestamp]
-  end
-
-  def valid_at
-    metadata[:valid_at]
-  end
-
-  def data
-    to_h.except(:event_id, :metadata)
-  end
-
-  def event_type
-    self.class.name
-  end
-
-  def message_id
-    event_id
-  end
-
-  def correlation_id
-    metadata[:correlation_id]
-  end
-
-  def correlation_id=(val)
-    metadata[:correlation_id] = val
-  end
-
-  def causation_id
-    metadata[:causation_id]
-  end
-
-  def causation_id=(val)
-    metadata[:causation_id] = val
-  end
-
-  def correlate_with(other_message)
-    self.correlation_id = other_message.correlation_id || other_message.message_id
-    self.causation_id   = other_message.message_id
-  end
-
-  def ==(other)
-    other.instance_of?(self.class) &&
-      other.event_id.eql?(event_id) &&
-      other.data.eql?(data)
-  end
-
-  alias_method :eql?, :==
+  include WithSchema
 end
