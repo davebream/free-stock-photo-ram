567f5950d8476a20aeba1d00de4ffea304cd6017
Dawid Leszczyński <dawid.leszczynski@gmail.com>
2021-11-29 14:13:27 +0100

    Move bounded contexts to contexts directory

--- config/initializers/zeitwerk.rb
+++ config/initializers/zeitwerk.rb
@@ -2,8 +2,8 @@ SUBDOMAINS = %w(reviewing file_processing tagging copyright_checking publishing)
 
 Rails.autoloaders.each do |autoloader|
   SUBDOMAINS.each do |sub|
-    events_dir = Rails.root.join("#{sub}/lib/#{sub}/events")
-    commands_dir = Rails.root.join("#{sub}/lib/#{sub}/commands")
+    events_dir = Rails.root.join("contexts/#{sub}/lib/#{sub}/events")
+    commands_dir = Rails.root.join("contexts/#{sub}/lib/#{sub}/commands")
 
     autoloader.collapse(events_dir)
     autoloader.collapse(commands_dir)
---
+++ contexts/tagging/spec/spec_helper.rb
@@ -0,0 +1,1 @@
+require_relative '../../../spec/rails_helper'
