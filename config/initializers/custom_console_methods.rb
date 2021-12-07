66bc69ae561ed4339887575f4b2da76f8f7c4b5c
Dawid Leszczyński <dawid.leszczynski@gmail.com>
2021-12-07 16:37:34 +0100

    Modify console methods

--- config/initializers/custom_console_methods.rb
+++ config/initializers/custom_console_methods.rb
@@ -1,15 +1,17 @@
 module CustomConsoleMethods
-  def event_store
-    cqrs.event_store
-  end
-
-  def command_bus
-    Rails.configuration.command_bus
-  end
-
   def cqrs
     Rails.configuration.cqrs
   end
+
+  def last_events(limit = 10)
+    event_store.read.backward.limit(limit).to_a
+  end
+
+  def last_event
+    last_events(1).first
+  end
+
+  delegate :event_store, :command_bus, to: :cqrs
 end
 
 require 'rails/console/helpers'
