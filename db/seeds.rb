0398be5d3beea986b9341b048f00440b51c65498
Dawid Leszczyński <dawid.leszczynski@gmail.com>
2021-12-07 18:57:28 +0100

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+ActiveRecord::Base.transaction do
+  Dir.glob(Rails.root.join('db/seeds/images/*')).each do |f|
+    file = File.open(f)
+    photo_id = SecureRandom.uuid
+    uploading_service = ::UploadImage.new(photo_id, file)
+    Uploading::UploadImage.new(uploading_service).call(photo_id)
+  end
+end
